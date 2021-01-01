82d0f2a6099093634411672554fc41396c66739c
ofir assulin <rarzip50@gmail.com>
2021-01-01 16:14:33 +0200

    last one for today hopfully

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path");
+const bodyParser = require("body-parser");
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
