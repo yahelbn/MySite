69e9229aed9281e8298dfa8280a79af9876bb47d
yahelbn <yahelbn3123@gmail.com>
2021-02-12 13:53:09 +0200

    creating more screens

--- client/src/Application/components/ApplicationScreens/Customers/SearchCustomer/index.js
+++ client/src/Application/components/ApplicationScreens/Customers/SearchCustomer/index.js
@@ -1,5 +1,5 @@
 import { text } from "body-parser";
-import React from "react";
+import React, { useState } from "react";
 
 import {
   FormWrap,
@@ -11,14 +11,52 @@ import {
   Form,
   Container,
   TextHeader,
+  CompaniesContainer,
+  CompanyRow,
+  ColumnDiv,
+  RowDiv,
+  FormLabel,
 } from "./SearchCustomerElements";
+
+import { FiUserPlus } from "react-icons/fi";
+
 const SearchCustomer = (props) => {
   const content = props.dataLanguages.searchcustomer;
+  const [searchWord, setSearchWord] = useState("");
+  const [arrayComp, setArrayComp] = useState("");
+  const [fields, setFields] = useState("");
+
+  //Example array of companies
+  const arrayTempCompanies = [
+    {
+      name: "Shen-Zen",
+      cid: "12345678",
+      poc: [
+        { name: "iris", telephone: "054-6609925", email: "iris@gmail.com" },
+        { name: "rafi", telephone: "054-6609930", email: "rafi@gmail.com" },
+      ],
+    },
+    {
+      name: "Shen-Zen",
+      cid: "12345678",
+      poc: [
+        { name: "iris", telephone: "054-6609925", email: "iris@gmail.com" },
+        { name: "rafi", telephone: "054-6609930", email: "rafi@gmail.com" },
+      ],
+    },
+  ];
 
   const urlAddCustomer = window.location.href.replace(
     "searchcustomer",
     "addcustomer"
   );
+
+  const searchCompany = () => {
+    console.log(searchWord);
+    setArrayComp(searchWord);
+    setFields(arrayTempCompanies);
+  };
+
   return (
     <Container>
       <FormWrap>
@@ -28,12 +66,68 @@ const SearchCustomer = (props) => {
           <TextHeader>{""}</TextHeader>
 
           <FormInput
+            list="companies"
             type={text}
             placeholder={content.inputplaceholder}
             required
+            onChange={(e) => setSearchWord(e.target.value)}
           />
+          <datalist id="companies">
+            <option>Shen-zen</option> <option>Clalit</option>
+            <option>Dr.ahdot</option>
+          </datalist>
 
-          <FormButton type="submit">{content.button}</FormButton>
+          <FormButton type="submit" onClick={() => searchCompany()}>
+            {content.button}
+          </FormButton>
+          {fields && (
+            <CompaniesContainer>
+              {" "}
+              {fields.map((field, idx) => {
+                return (
+                  <div
+                    style={{ width: "100%" }}
+                    dir={content.rtl ? "rtl" : "ltr"}
+                    key={`${field}-${idx}`}
+                  >
+                    <CompanyRow>
+                      <ColumnDiv>
+                        <FormLabel dark={true} htmlFor="for">
+                          {field.name}
+                        </FormLabel>
+                      </ColumnDiv>
+                      <ColumnDiv>
+                        <FormLabel dark={true} htmlFor="for">
+                          {field.cid}
+                        </FormLabel>
+                      </ColumnDiv>
+                      <ColumnDiv>
+                        <FormLabel dark={true} htmlFor="for">
+                          {field.cid}
+                        </FormLabel>
+                      </ColumnDiv>
+                      <ColumnDiv>
+                        <button
+                          type="button"
+                          style={{
+                            cursor: "pointer",
+                            WebkitBorderRadius: "20px",
+                          }}
+                        >
+                          <FormLabel dark={true} htmlFor="for">
+                            add Customer
+                          </FormLabel>
+
+                          <FiUserPlus />
+                        </button>
+                      </ColumnDiv>
+                    </CompanyRow>
+                  </div>
+                );
+              })}
+            </CompaniesContainer>
+          )}
+
           <Text>
             {content.text} <a href={urlAddCustomer}>{content.link}</a>
           </Text>
--- client/src/Application/components/InitOrJoinCompany/index.js
+++ client/src/Application/components/InitOrJoinCompany/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FormWrap,
   FormH1,
@@ -11,9 +11,20 @@ import {
   ColumnDiv,
   Container,
   FormSelect,
+  RowDivPoc,
+  AddButton,
 } from "./InitOrJoinElements";
 
+import UseAnimations from "react-useanimations";
+// EVERY ANIMATION NEEDS TO BE IMPORTED FIRST -> YOUR BUNDLE WILL INCLUDE ONLY WHAT IT NEEDS
+import trash2 from "react-useanimations/lib/trash2";
+import { AiOutlinePlus } from "react-icons/ai";
+
 const InitOrJoinCompany = ({ content }) => {
+  const [fields, setFields] = useState([
+    { firstname: null, lastname: null, email: null },
+  ]);
+
   const renderOptions = content.optionsSelect.map((option, index) => {
     if (index === 0) {
       return (
@@ -31,6 +42,26 @@ const InitOrJoinCompany = ({ content }) => {
       );
     }
   });
+
+  function handleChange(i, type, event) {
+    const values = [...fields];
+    if (type === 0) values[i].firstname = event.target.value;
+    if (type === 1) values[i].lastname = event.target.value;
+    if (type === 2) values[i].email = event.target.value;
+    setFields(values);
+  }
+  const handleAdd = () => {
+    const values = [...fields];
+    values.push({ firstname: null, lastname: null, email: null });
+    setFields(values);
+  };
+
+  function handleRemove(i) {
+    const values = [...fields];
+    values.splice(i, 1);
+    setFields(values);
+  }
+
   return (
     <>
       <Container>
@@ -56,6 +87,56 @@ const InitOrJoinCompany = ({ content }) => {
 
             <FormLabel htmlFor="for">{content.formlabel4}</FormLabel>
             <FormInput type={content.forminput4} required />
+
+            {fields.map((field, idx) => {
+              return (
+                <div dir={content.rtl ? "rtl" : "ltr"} key={`${field}-${idx}`}>
+                  <RowDivPoc>
+                    <ColumnDiv>
+                      <FormLabel htmlFor="for">{content.formlabel5}</FormLabel>
+                      <FormInput
+                        type={content.forminput5}
+                        required
+                        value={field.firstname || ""}
+                        onChange={(e) => handleChange(idx, 0, e)}
+                      />
+                    </ColumnDiv>
+                    <ColumnDiv>
+                      <FormLabel htmlFor="for">{content.formlabel6}</FormLabel>
+                      <FormInput
+                        type={content.forminput6}
+                        required
+                        value={field.lastname || ""}
+                        onChange={(e) => handleChange(idx, 1, e)}
+                      />
+                    </ColumnDiv>
+                    <ColumnDiv>
+                      <FormLabel htmlFor="for">{content.formlabel7}</FormLabel>
+                      <FormInput
+                        type={content.forminput7}
+                        required
+                        value={field.email || ""}
+                        onChange={(e) => handleChange(idx, 2, e)}
+                      />
+                    </ColumnDiv>
+                    <ColumnDiv>
+                      <button type="button" onClick={() => handleRemove(idx)}>
+                        <UseAnimations animation={trash2} />
+                      </button>
+                    </ColumnDiv>
+                  </RowDivPoc>
+                </div>
+              );
+            })}
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <AddButton type="submit" onClick={handleAdd}>
+                <div style={{ fontSize: "15px" }}>{content.buttonContent2}</div>
+                <AiOutlinePlus
+                  color={"#1b1924"}
+                  style={{ marginLeft: "3px", marginRight: "3px" }}
+                />
+              </AddButton>
+            </div>
 
             <FormButton type="submit">{content.buttonContent1}</FormButton>
           </Form>
