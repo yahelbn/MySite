7971906e65f31f13f520b1db04618c95bf2a799a
Yahel Barnoam <55202944+yahelbn@users.noreply.github.com>
2021-03-19 17:26:23 +0200

    Delete client directory

--- client/src/Application/components/ProfileBar/ProfileBarElements.js
+++
@@ -1,3 +0,0 @@
-import styled from "styled-components";
-
-export const DropDown = styled.div``;
--- client/src/Application/components/ProfileBar/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import { Dropdown } from "react-bootstrap";
-import {} from "../ProfileBar/ProfileBarElements";
-// TODO: This is missing functionality for sub-menu here from SUI core examples.
-// The "Publish To Web" item should contain a sub-menu.
-
-const DropdownExampleDropdown = () => <div></div>;
-
-export default DropdownExampleDropdown;
